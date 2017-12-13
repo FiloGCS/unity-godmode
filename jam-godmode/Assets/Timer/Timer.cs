8fa273fbad35d0e4841df4d2ef8d90ac2035334a
FiloGCS <guillermo.couceiro@gmail.com>
2017-12-13 05:08:38 +0100

    Timer - New Module

---
+++ jam-godmode/Assets/Timer/Timer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    //Starting time in seconds
+    public float TIMER_STARTING_VALUE = 300;
+    //Time scale and direction
+    public float TIMER_SCALE = -1;
+
+    public float time;
+    bool running = false;
+
+    void Update() {
+        //TODO Output value somewhere
+    }
+
+    void FixedUpdate() {
+        if (running) {
+            time += Time.fixedDeltaTime * TIMER_SCALE;
+        }
+    }
+
+    //Sets the timer to the starting value
+    public void ResetTimer() {
+        time = TIMER_STARTING_VALUE;
+    }
+
+    //Pauses the timer
+    public void PauseTimer() {
+        running = false;
+    }
+
+    //Unpauses the timer
+    public void UnpauseTimer() {
+        running = true;
+    }
+
+    //Pauses/unpauses the timer
+    public void PauseTimerToggle() {
+        running = !running;
+    }
+
+
+    //Resets and Unpauses the timer
+    public void StartTimer() {
+        ResetTimer();
+        UnpauseTimer();
+    }
+
+    //Resets and Pauses the timer
+    public void StopTimer() {
+        ResetTimer();
+        PauseTimer();
+    }
+
+}
