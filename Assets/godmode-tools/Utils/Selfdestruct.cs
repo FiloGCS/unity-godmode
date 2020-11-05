2b947120176cb9fffa6d86b0ab15b05ec3690813
Guillermo CS <guillermo.couceiro@gmail.com>
2020-11-05 03:50:29 +0100

    Selfdestruct

---
+++ Assets/godmode-tools/Utils/Selfdestruct.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Godmode/Selfdestruct")]
+public class Selfdestruct : MonoBehaviour {
+
+    [SerializeField]
+    private float lifetime = 0f;
+
+    private void Start() {
+        if (lifetime > 0) {
+            TimedKill(lifetime);
+        }
+    }
+
+    public void TimedKill(float ttl) {
+        StartCoroutine(WaitAndKill(ttl));
+    }
+
+    private IEnumerator WaitAndKill(float t) {
+        yield return new WaitForSeconds(t);
+        Kill();
+    }
+
+    public void Kill() {
+        Destroy(this.gameObject);
+    }
+}
